aa017c5bf7bdd66803e54393050838fc22250071
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-20 22:35:01 +0530

    Add files via upload

---
+++ Strings/Easy_Reverse.java
@@ -0,0 +1,15 @@
+class Easy_Reverse {
+    public void reverseString(char[] s) {
+        char[] result = new char[s.length];
+        int idx = 0;
+
+        for(int i = s.length - 1; i >= 0; i--) {
+            result[idx++] = s[i];
+        }
+
+        for(int i = 0; i < s.length; i++) {
+            s[i] = result[i];
+        }
+	System.out.println(Arrays.tostring(s));
+    }
+}
