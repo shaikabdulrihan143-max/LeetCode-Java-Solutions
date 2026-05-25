7a90dd5eb4b5ee0bdeeab57c9e44eeb7736a4dc9
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-25 23:39:39 +0530

    Add files via upload

---
+++ TwoPointers/Easy_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+class Easy_RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int idx=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[idx]=nums[i];
+                idx++;
+            }
+        }
+        
+        
+        return idx;
+    }
+}
