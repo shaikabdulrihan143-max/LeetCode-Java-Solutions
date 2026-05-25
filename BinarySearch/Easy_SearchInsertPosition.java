aa88a762f4272c9099407cfc930161dde84b6ac6
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-26 00:55:15 +0530

    Add files via upload

---
+++ BinarySearch/Easy_SearchInsertPosition.java
@@ -0,0 +1,20 @@
+class Easy_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=(low+high)/2;
+        while(low<=high){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            mid=(low+high)/2;
+        }
+        return low;
+    }
+}
