1e215f3734101f68672e5eda8c6661c4a4338406
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-27 11:48:31 +0530

    Add files via upload

---
+++ Backtracking/Medium_LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,20 @@
+class Medium_LetterCombinationsOfPhoneNumber {
+    public String keypad[]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public void helper(String digits,List<String> ans,String combination,int idx){
+        if(idx==digits.length()){
+            ans.add(combination);
+            return;
+        }
+        char ch=digits.charAt(idx);
+        String mapping=keypad[ch-'2'];
+        for(int i=0;i<mapping.length();i++){
+            helper(digits,ans,combination+mapping.charAt(i),idx+1);
+
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        List<String> ans=new ArrayList<>();
+        helper(digits,ans,"",0);
+        return ans;
+    }
+}
