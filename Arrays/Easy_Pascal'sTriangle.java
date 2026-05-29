e42a0dcc1b9dc5512a8b276887d3eb21c038158a
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 20:43:22 +0530

    Add files via upload

---
+++ Arrays/Easy_Pascal'sTriangle.java
@@ -0,0 +1,16 @@
+class Easy_Pascal'sTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=1;i<=numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            int num=1;
+            for(int j=1;j<=i;j++){
+                row.add(num);
+                num=num*(i-j)/(j);
+            }
+            ans.add(row);
+        }
+        return ans;
+
+    }
+}
