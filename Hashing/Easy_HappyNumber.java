2af1bee842f9c5d71bd4d7a543c7a27349359131
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 22:32:51 +0530

    Add files via upload

---
+++ Hashing/Easy_HappyNumber.java
@@ -0,0 +1,22 @@
+class Easy_HappyNumber {
+    public boolean helper(int n,HashSet<Integer> set){
+        if(n==1){
+            return true;
+        }
+        if(set.contains(n)){
+            return false;
+        }
+        int happy=0;
+        set.add(n);
+        while(n>0){
+            int d=n%10;
+            happy=happy+(d*d);
+            n=n/10;
+        }
+        return helper(happy,set);
+    }
+    public boolean isHappy(int n) {
+        HashSet<Integer> set=new HashSet<>();
+        return helper(n,set);
+    }
+}
