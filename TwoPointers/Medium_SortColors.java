1600c565e7324cff3dcf4b040b7468ac59afc702
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 16:23:40 +0530

    Update Medium_SortColors.java

--- TwoPointers/Medium_SortColors.java
+++ TwoPointers/Medium_SortColors.java
@@ -1,14 +1,32 @@
 class Easy_SortColors {
-    public void sortColors(int[] nums) {
-        int temp;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]>nums[j]){
-                    temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
+    public void sort(int nums[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        int pivot=nums[mid];
+        int left=si;
+        int right=ei;
+        while(left<=right){
+            while(nums[left]<pivot){
+                left++;
+            }
+            while(nums[right]>pivot){
+                right--;
+            }
+            if(left<=right){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
             }
         }
+        sort(nums,si,right);
+        sort(nums,left,ei);
+    }
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        sort(nums,0,n-1);
     }
 }
