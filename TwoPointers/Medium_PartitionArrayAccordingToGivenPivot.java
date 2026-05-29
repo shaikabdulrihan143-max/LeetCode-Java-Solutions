fe468327763e7bbfeb4d538a8ee3dd13f46e7c77
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 17:37:36 +0530

    Add files via upload

---
+++ TwoPointers/Medium_PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,23 @@
+class Medium_PartitionArrayAccordingToGivenPivot {
+
+    public int[] pivotArray(int[] nums, int pivot) {
+        int idx=0;
+        int result[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                result[idx++]=nums[i];
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==pivot){
+                result[idx++]=nums[i];
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>pivot){
+                result[idx++]=nums[i];
+            }
+        }
+        return result;
+    }
+}
