ca94e5710dac4bd17230fd12e9c0075ebcab6cdf
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 13:19:19 +0530

    Add files via upload

---
+++ Sorting/Medium_SortAnArray.java
@@ -0,0 +1,39 @@
+class Medium_SortAnArray {
+    public void merge(int nums[],int si,int mid,int ei){
+        int newArray[]=new int[ei+1-si];
+        int idx=0;
+        int idx1=si;
+        int idx2=mid+1;
+        while(idx1<=mid && idx2<=ei){
+            if(nums[idx1]<=nums[idx2]){
+                newArray[idx++]=nums[idx1++];
+            }
+            else{
+                newArray[idx++]=nums[idx2++];
+            }
+        }
+        while(idx1<=mid){
+            newArray[idx++]=nums[idx1++];
+        }
+        while(idx2<=ei){
+            newArray[idx++]=nums[idx2++];
+        }
+        for(int i=si,j=0;i<=ei;i++,j++){
+            nums[i]=newArray[j];
+        }
+    }
+    public void divide(int nums[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        divide(nums,si,mid);
+        divide(nums,mid+1,ei);
+        merge(nums,si,mid,ei);
+    }
+    public int[] sortArray(int[] nums) {
+        int n=nums.length;
+        divide(nums,0,n-1);
+        return nums;
+    }
+}
