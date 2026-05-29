d573303d96738bb1517ee8c8c4f7f1a40576961e
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 13:04:08 +0530

    Update Easy_MergeSortedArray.java

--- TwoPointers/Easy_MergeSortedArray.java
+++ TwoPointers/Easy_MergeSortedArray.java
@@ -1,15 +1,25 @@
-import java.util.*;
 class Easy_MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int newArray[]=new int[m+n];
         int idx=0;
-        int count=0;
-        for(int i=0;i<m;i++){
-            nums1[idx++]=nums1[i];
+        int idx1=0;
+        int idx2=0;
+        while(idx1<=m-1 && idx2<=n-1){
+            if(nums1[idx1]<=nums2[idx2]){
+                newArray[idx++]=nums1[idx1++];
+            }
+            else{
+                newArray[idx++]=nums2[idx2++];
+            }
         }
-        count=0;
-        for(int i=0;i<n;i++){
-            nums1[idx++]=nums2[i];
-        } 
-        Arrays.sort(nums1);
+        while(idx1<=m-1){
+            newArray[idx++]=nums1[idx1++];
+        }
+        while(idx2<=n-1){
+            newArray[idx++]=nums2[idx2++];
+        }
+        for(int i=0;i<newArray.length;i++){
+            nums1[i]=newArray[i];
+        }
     }
 }
