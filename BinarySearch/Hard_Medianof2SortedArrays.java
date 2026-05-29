5f2fcc4bd079bb9b226900b5d2bc576e617e8248
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-05-29 16:04:51 +0530

    Update Hard_Medianof2SortedArrays.java

--- BinarySearch/Hard_Medianof2SortedArrays.java
+++ BinarySearch/Hard_Medianof2SortedArrays.java
@@ -1,29 +1,32 @@
 class Hard_Medianof2SortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int result[]=new int[nums1.length+nums2.length];
-        int idx=0,temp;
-        for(int i=0;i<nums1.length;i++){
-            result[idx++]=nums1[i];
-        }
-        for(int i=0;i<nums2.length;i++){
-            result[idx++]=nums2[i];
-        }
-        for(int i=0;i<result.length;i++){
-            for(int j=i+1;j<result.length;j++){
-                if(result[i]>result[j]){
-                    temp=result[i];
-                    result[i]=result[j];
-                    result[j]=temp;
-                }
+        int m=nums1.length;
+        int n=nums2.length;
+        int size=m+n;
+        int result[]=new int[size];
+        int idx=0;
+        int idx1=0;
+        int idx2=0;
+        while(idx1<m && idx2<n){
+            if(nums1[idx1]<=nums2[idx2]){
+                result[idx++]=nums1[idx1++];
+            }
+            else{
+                result[idx++]=nums2[idx2++];
             }
         }
-        int low=0,high=result.length-1;
-        int mid=(low+high)/2;
-        if(result.length%2==1){
-            return result[mid];
+        while(idx1<m){
+            result[idx++]=nums1[idx1++];
+        }
+        while(idx2<n){
+            result[idx++]=nums2[idx2++];
+        }
+        int mid=size/2;        
+        if(size%2==1){
+            return (double)result[mid];
         }
         else{
-            return (double)(result[mid]+result[mid+1])/2;
+            return (result[mid]+result[mid-1])/2.0;
         }
     }
 }
