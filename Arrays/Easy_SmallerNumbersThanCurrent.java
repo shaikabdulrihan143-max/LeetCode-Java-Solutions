7fcae76933b2bd181d288bb004af92857813457b
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-01 16:43:39 +0530

    Add files via upload

---
+++ Arrays/Easy_SmallerNumbersThanCurrent.java
@@ -0,0 +1,17 @@
+class Easy_SmallerNumbersThanCurrent {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int ans[]=new int[nums.length];
+        int count=0;
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                }
+            }
+            ans[idx++]=count;
+        }
+        return ans;
+    }
+}
