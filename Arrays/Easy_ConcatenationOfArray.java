2d5a0c871f5337c1d15afa2305282ea461d16ecf
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-01 16:02:03 +0530

    Add files via upload

---
+++ Arrays/Easy_ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+class Easy_ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[2*nums.length];
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            ans[idx++]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            ans[idx++]=nums[i];
+        }
+        return ans;
+    }
+}
