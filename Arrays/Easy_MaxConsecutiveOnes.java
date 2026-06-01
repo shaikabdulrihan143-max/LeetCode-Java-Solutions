3e01559ae2c10adf9475f7cd31746628b3f19cb2
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-01 15:44:20 +0530

    Add files via upload

---
+++ Arrays/Easy_MaxConsecutiveOnes.java
@@ -0,0 +1,16 @@
+class Easy_MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max,count);
+            }
+            else{
+                count=0;
+            }
+        }
+        return max;
+    }
+}
