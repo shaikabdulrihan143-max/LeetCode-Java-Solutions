880fa2e9a4d774330899da29e8e7210ae771225f
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-01 15:56:09 +0530

    Add files via upload

---
+++ Arrays/Easy_ShuffleTheArray.java
@@ -0,0 +1,11 @@
+class Easy_ShuffleTheArray {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[]=new int[2*n];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            ans[idx++]=nums[i];
+            ans[idx++]=nums[i+n];
+        }
+        return ans;
+    }
+}
