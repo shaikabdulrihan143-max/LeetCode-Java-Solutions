65b4facf60177b91b77fdb53e6654289b86a6334
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-05 22:28:45 +0530

    Add files via upload

---
+++ Hashing/Easy_IntersectionOfTwoArrays.java
@@ -0,0 +1,19 @@
+class Easy_IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1=new HashSet<>();
+        HashSet<Integer> set2=new HashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            set2.add(nums2[i]);
+        }
+        set1.retainAll(set2);
+        int intersection[]=new int[set1.size()];
+        int idx=0;
+        for(int num :set1){
+            intersection[idx++]=num;
+        }
+        return intersection;
+    }
+} 
