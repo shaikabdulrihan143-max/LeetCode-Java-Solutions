1badd6597d1fb3ba1ab4eaf7217914d5cb89d07e
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-12 17:38:34 +0530

    Update Easy_NumberOf1Bits.java

--- BitManipulation/Easy_NumberOf1Bits.java
+++ BitManipulation/Easy_NumberOf1Bits.java
@@ -1,16 +1,9 @@
 class Easy_NumberOf1Bits {
     public int hammingWeight(int n) {
-        String binary="";
-        while(n>0){
-            int d=n%2;
-            binary=binary+d;
-            n=n/2;
-        }
         int count=0;
-        for(int i=0;i<binary.length();i++){
-            if(binary.charAt(i)=='1'){
-                count++;
-            }
+        while(n>0){
+            n=n&(n-1);
+            count++;
         }
         return count;
     }
