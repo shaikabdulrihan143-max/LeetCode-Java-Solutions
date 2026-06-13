dbaedad9883777dd586121fc13d12dd0d3becb12
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 16:49:01 +0530

    Add files via upload

---
+++ Arrays/Easy_MissingNumber.java
@@ -0,0 +1,16 @@
+class Easy_MissingNumber {
+    public int missingNumber(int[] nums) {
+        boolean m[]=new boolean[nums.length+1];
+        for(int i=0;i<nums.length;i++){
+            if(m[nums[i]]==false){
+                m[nums[i]]=true;
+            }
+        }
+        for(int i=0;i<m.length;i++){
+            if(m[i]==false){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
