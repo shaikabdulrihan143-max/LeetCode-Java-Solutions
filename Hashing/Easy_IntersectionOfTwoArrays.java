76c1d2b2698a1a647de27d035d29a18c4eb17df4
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-17 17:34:12 +0530

    Update Easy_IntersectionOfTwoArrays.java

--- Hashing/Easy_IntersectionOfTwoArrays.java
+++ Hashing/Easy_IntersectionOfTwoArrays.java
@@ -1,4 +1,4 @@
-class Easy_IntersectionOfTwoArrays {
+class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set1=new HashSet<>();
         HashSet<Integer> set2=new HashSet<>();
